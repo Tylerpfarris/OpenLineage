2b3bc28bd674eed552fa09ba1d937f7f64300d0c
Michael Robinson <merobi@gmail.com>
2022-06-03 11:31:10 -0400

    fix issue in setup.py files in airflow, dagster, common integrations
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- integration/dagster/setup.py
+++ integration/dagster/setup.py
@@ -1,6 +1,6 @@
 # Copyright 2018-2022 contributors to the OpenLineage project
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
